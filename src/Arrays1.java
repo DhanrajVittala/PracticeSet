50694a739d58ef9d26bf3579ba2beffb782b530b
DHANRAJ VITTALA <142311588+DhanrajVittala@users.noreply.github.com>
2024-02-13 10:13:36 +0530

    5th commit

--- src/Arrays1.java
+++ src/Arrays1.java
@@ -4,12 +4,14 @@ public class Arrays1 {
 
 		int[] arr = { 5, 6, 8, 2, 1, 5, 9, 3 };
 		
-		int n = arr.length - 1;
-		int k=3;
+		int n = arr.length ;
+		int k=1;
+		k=k%n;
+		
 		
         // reverse the entire array in original array
 		//{3,9,5,1,2,8,6,5}
-		swapArray(arr, 0, n);
+		swapArray(arr, 0, n-1);
 		
 		//reverse the first 3 postion
 		//{5,9,3,1,2,8,6,5}
@@ -17,7 +19,7 @@ public class Arrays1 {
 		
 		//reverse the remaining array
 		//{5,9,3,5,6,8,2,1}
-		swapArray(arr,k,n);
+		swapArray(arr,k,n-1);
 
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
