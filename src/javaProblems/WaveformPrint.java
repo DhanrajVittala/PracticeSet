6b4b95adee5230b83cb9cc8ce90cbe686551510e
DHANRAJ VITTALA <142311588+DhanrajVittala@users.noreply.github.com>
2024-02-14 22:24:38 +0530

    Update WaveformPrint.java

--- src/javaProblems/WaveformPrint.java
+++ src/javaProblems/WaveformPrint.java
@@ -5,26 +5,26 @@ import java.util.Scanner;
 public class WaveformPrint {
 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		int n= sc.nextInt();
-		int m=sc.nextInt();
-		
-		int[][] arr= new int[n][m];
-		
-		for(int i=0;i<=n-1;i++) {
-			for(int j=0;j<=m-1;j++) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		int[][] arr = new int[n][m];
+
+		for (int i = 0; i <= n - 1; i++) {
+			for (int j = 0; j <= m - 1; j++) {
 				arr[i][j] = sc.nextInt();
 			}
 		}
-		for(int i=0;i<=n-1;i++) {
-			if(i%2==0) {
-			for(int j=0;j<=m-1;j++) {
-				System.out.print(arr[i][j]+" ");
-			}
-			}else {
-				for(int j=n-1;j>=0;j--) {
-					System.out.print(arr[i][j]+" ");
-			}
+		for (int i = 0; i <= n - 1; i++) {
+			if (i % 2 == 0) {
+				for (int j = 0; j <= m - 1; j++) {
+					System.out.print(arr[i][j] + " ");
+				}
+			} else {
+				for (int j = n - 1; j >= 0; j--) {
+					System.out.print(arr[i][j] + " ");
+				}
 			}
 			System.out.println();
 		}
